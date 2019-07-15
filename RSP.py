5a231a70d6754ac0975ecd1b67aa1f0451b8c157
KimKangYeon <kevin787@hanmail.net>
2019-07-15 21:25:22 +0900

    보자기를 냈을 경우

--- RSP.py
+++ RSP.py
@@ -1,2 +1,10 @@
 while True:
     print("가위바위보 중 하나를 선택하세요")
+
+        if user=='보':
+        if random.choice(["가위","바위","보"])=="가위":
+            print("패")
+        elif random.choice(["가위","바위","보"])=="바위":
+            print("승")
+        else:
+            print("무승부")
