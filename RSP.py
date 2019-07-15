f404e605a512e83f444c6932c506448d2be5cea0
elksha <lksia12@naver.com>
2019-07-15 21:42:45 +0900

    190715

--- RSP.py
+++ RSP.py
@@ -1,6 +1,5 @@
 import random
 while True:
-
     user = input("가위바위보를 하세요:")
     if user=='바위':
         if random.choice(["가위","바위","보"]=="가위"):
@@ -9,13 +8,3 @@ while True:
             print("무승부")
         else:
             print("패")
-   
- 
-
-    if user =='가위':
-        if random.choice(["가위", "바위", "보"]) == "가위":
-            print("무승부")
-        elif random.choice(["가위", "바위", "보"]) == "바위":
-            print("패")
-        else:
-            print("승")
